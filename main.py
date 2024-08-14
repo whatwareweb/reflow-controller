bb8855ae405d494c4a76ceb986d653e2973ef9eb
whatwareweb <59752545+whatwareweb@users.noreply.github.com>
2024-08-14 09:03:51 -0500

    Implement more of profile selection

--- main.py
+++ main.py
@@ -17,9 +17,15 @@ lcd = I2cLcd(lcd_i2c, LCD_I2C_ADDR, 2, 16)
 
 
 menu = None
+profiles = None
 
 with open("menu.json") as f:
     menu = json.load(f)["children"]
+
+with open("profiles.json") as f:
+    profiles = json.load(f)["profiles"]
+    profiles.append({"name": "Back"})
+
 
 global submenu
 global menu_pos
@@ -55,12 +61,10 @@ menu_functions = {
     "back": menu_back,
     "backlight_on": lcd.backlight_on,
     "backlight_off": lcd.backlight_off,
-    "clear_vars": clear_vars,
-    "select_profile": print
-
+    "clear_vars": clear_vars
 }
 
-process_status = "main_menu"
+process_status = "profile_select"
 
 async def menu_control():
     global submenu
@@ -93,8 +97,10 @@ async def menu_control():
                 elif (("function" in submenu[-1][pos + cursor_pos].keys()) and (len(submenu[-1][pos + cursor_pos]["function"]) > 0)):
                     for function in (submenu[-1][pos + cursor_pos]["function"]):
                         menu_functions[function]()
-            
             update_menu()
+            
+        else:
+            await uasyncio.sleep_ms(0)
 
 async def profile_select_menu():
     global menu_pos
@@ -106,9 +112,9 @@ async def profile_select_menu():
             lcd.move_to(0, cursor_pos)
             lcd.putchar(">")
             lcd.move_to(1, 0)
-            lcd.putstr(submenu[-1][pos]["name"])
+            lcd.putstr(profiles[pos]["name"])
             lcd.move_to(1, 1)
-            lcd.putstr(submenu[-1][pos + 1]["name"])
+            lcd.putstr(profiles[pos + 1]["name"])
             input = await keypad.get_input()
 
             if (int(input) == 8):
@@ -118,16 +124,24 @@ async def profile_select_menu():
                 cursor_pos -= 1
 
             if (int(input) == 5):
-                if (("children" in submenu[-1][pos + cursor_pos].keys()) and (len(submenu[-1][pos + cursor_pos]["children"]) > 0)):
-                    submenu.append(submenu[-1][pos + cursor_pos]["children"])
-                    menu_pos.append([pos, cursor_pos])
-                    pos = 0
-                    cursor_pos = 0
-                elif (("function" in submenu[-1][pos + cursor_pos].keys()) and (len(submenu[-1][pos + cursor_pos]["function"]) > 0)):
-                    for function in (submenu[-1][pos + cursor_pos]["function"]):
-                        menu_functions[function]()
-            
+                if (profiles[pos + cursor_pos]["name"] == "Back"):
+                    process_status = "main_menu"
+                else:
+                    lcd.clear()
+                    lcd.move_to(0, 0)
+                    lcd.putstr(profiles[pos + cursor_pos]["name"])
+                    lcd.move_to(0, 1)
+                    lcd.putstr("A:Start, B:Exit")
+                    input = await keypad.get_input()
+                    if (input == 'A'):
+                        #placeholder
+                    elif (input == 'B')
+
+
             update_menu()
+
+        else:
+            await uasyncio.sleep_ms(0)
 
 
 def update_menu():
@@ -156,4 +170,5 @@ def update_menu():
 
 menu_loop = uasyncio.get_event_loop()
 menu_loop.create_task(menu_control())
+menu_loop.create_task(profile_select_menu())
 menu_loop.run_forever()
